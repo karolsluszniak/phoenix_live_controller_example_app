49a1d7aca7902a01f6d4b878a36b24ed4347d626
Karol Słuszniak <ksluszniak@gmail.com>
2020-04-13 11:18:04 +0200

    mix phx.gen.html Blog Article articles title

--- lib/my_app_web/router.ex
+++ lib/my_app_web/router.ex
@@ -17,6 +17,7 @@ defmodule MyAppWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/articles", ArticleController
   end
 
   # Other scopes may use custom stacks.
